c1f08d0e5acf57e1bfddac50794ae355c7524f6c
X <sabinarai@x.broadband>
2024-09-11 14:41:44 +0100

    feat: integrated swagger ui

--- src/main/java/com/northcoders/example/RecordShop/RecordShopApplication.java
+++ src/main/java/com/northcoders/example/RecordShop/RecordShopApplication.java
@@ -2,13 +2,25 @@ package com.northcoders.example.RecordShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class RecordShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecordShopApplication.class, args);
-		System.out.println("hello");;
+
+	}
+
+	@Bean
+	public OpenAPI todoApiInfo(){
+		return new OpenAPI()
+				.info(new Info().title("record shop")
+						.version("v1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
